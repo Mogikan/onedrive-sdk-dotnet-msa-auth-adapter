dadd9b8633fe1ab06e4f4335f5ac5458d54b167f
Mogikan <gcnew@yandex.ru>
2021-11-15 11:49:56 +0700

    serialization fix

--- src/OneDrive.Sdk.Authentication.Common/OAuthHelper.cs
+++ src/OneDrive.Sdk.Authentication.Common/OAuthHelper.cs
@@ -1,4 +1,4 @@
-﻿// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
@@ -285,9 +285,8 @@ namespace Microsoft.OneDrive.Sdk.Authentication
             using (var authResponse = await httpProvider.SendAsync(httpRequestMessage).ConfigureAwait(false))
             using (var responseStream = await authResponse.Content.ReadAsStreamAsync().ConfigureAwait(false))
             {
-                var responseValues =
-                    httpProvider.Serializer.DeserializeObject<IDictionary<string, string>>(
-                        responseStream);
+                var rawValues = httpProvider.Serializer.DeserializeObject<Dictionary<string, object>>(responseStream);
+                var responseValues = rawValues.ToDictionary(item => item.Key, item => item.Value.ToString());
 
                 if (responseValues != null)
                 {
